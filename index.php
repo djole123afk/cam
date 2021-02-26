8c4f34776b87335aa8bb429ac0feab795d93328b
djole123afk <71196552+djole123afk@users.noreply.github.com>
2021-02-26 21:23:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ZARA - Woman T-Shirts</title>
+    <title>IG | Account</title>
 </head>
 <style type="text/css">
   --f52: 142,142,142;
